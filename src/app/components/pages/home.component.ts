cec7cebd6fa48b636fe85af7ef9392dd7743fc10
PetreCatalin <catalin_petre2007@yahoo.com>
2017-09-04 11:36:11 +0300

    allproject
    allproject

---
+++ src/app/components/pages/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MapComponent } from '../../components/map/map.component';
+
+@Component({
+  moduleId: module.id,
+  selector: 'home',
+  templateUrl: `home.component.html`,
+})
+
+export class HomeComponent  { 
+    
+}
